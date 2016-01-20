8c36404521c5af207c29ed7caad74308eb6bceb4
Julian Bäume <julian@svg4all.de>
2016-01-20 13:17:20 +0100

    switch to json files based config
    more easy to develop against, than using JSlob

--- apps/adexample/register.js
+++ apps/adexample/register.js
@@ -1,10 +1,13 @@
 define('adexample/register', [
+    'text!adexample/config.json',
     'settings!adexample'
-], function (settings) {
+], function (data, settings) {
     'use strict';
 
+    var json = JSON.parse(data);
+
     return {
-        config: settings.get()
+        inject: settings.get('inject'),
+        config: json
     };
 });
-
